6552d3b7c5ddf503bd38ba96c6f2f8ed19ba9e24
mike vallano <mikevallano@gmail.com>
2020-02-19 19:51:52 -0600

    Bare bones auth via twitter

--- Gemfile
+++ Gemfile
@@ -20,6 +20,8 @@ gem 'jbuilder', '~> 2.7'
 
 gem 'devise'
 gem 'twitter'
+gem 'omniauth'
+gem 'omniauth-twitter'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use Active Model has_secure_password
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,23 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :confirmable, :lockable, :timeoutable, and :trackable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: %i[twitter]
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user| # TODO check what provider returns
+      user.email = auth.info.email || '' # to bypass not null constraint on the db
+      user.password = Devise.friendly_token[0, 20]
+      user.username = auth.info.nickname
+
+      # image, etc.
+
+      # user.skip_confirmation! # if using confirmable
+    end
+  end
+
+  def email_required?
+    false # some twitter users won't have an email. should probably distinguish twitter auth users. TODO
+  end
 end
