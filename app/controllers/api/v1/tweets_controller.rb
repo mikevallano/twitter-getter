bb924342de3ea73e48d4c4cbd58f113abc17a72b
mike vallano <mikevallano@gmail.com>
2020-04-21 18:45:21 -0500

    Fetch with max_id

--- app/controllers/api/v1/tweets_controller.rb
+++ app/controllers/api/v1/tweets_controller.rb
@@ -3,9 +3,9 @@ class Api::V1::TweetsController < Api::V1::BaseController
     tag = Tag.find_by(name: params[:tag])
 
     tweets = if tag
-      tag.liked_tweets.where(user: current_user).includes(:tags).ordered.first(5)
+      tag.liked_tweets.where(user: current_user).includes(:tags).ordered.first(25)
     else
-      current_user.liked_tweets.includes(:tags).ordered.first(5)
+      current_user.liked_tweets.includes(:tags).ordered.first(25)
     end
 
     render json: LikedTweetSerializer.new(tweets)
--- app/services/twitter_api.rb
+++ app/services/twitter_api.rb
@@ -10,7 +10,7 @@ class TwitterApi
     end
   end
 
-  def get_favorites(username:, count: 200, since_id: nil, max_id: nil)
-    client.favorites(username, count: count) # TODO, need to account for since & max. the api doesn't want the param unless it's valid
+  def get_favorites(username:, count: 200, since_id: '1', max_id: nil)
+    client.favorites(username, count: count, since_id: since_id) # TODO, need to account for since & max. the api doesn't want the param unless it's valid
   end
 end
