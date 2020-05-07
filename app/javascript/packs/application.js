5f913238a9ae0db4fa5cf8667fd5dbf995210498
mike vallano <mikevallano@gmail.com>
2020-05-07 07:26:32 -0500

    Trying to get deploy to work Add Procfile, tweak readme
    Use stylesheet_pack_tag instead of stylesheet_link_tag
    
    Try extract css in webpacker
    
    Comment out pages stylesheet
    
    Require path to sass file

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ require("@rails/activestorage").start()
 require("channels")
 
 import 'bootstrap/dist/css/bootstrap.min.css'
+require('../../assets/stylesheets/pages.scss')
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
