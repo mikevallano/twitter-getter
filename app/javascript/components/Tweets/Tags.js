470c894dd9f0237c83a6e081df65f9b1e6aef1b0
mike vallano <mikevallano@gmail.com>
2022-03-31 14:56:37 -0500

    Refactor Tags to functional component

--- app/javascript/components/Tweets/Tags.js
+++ app/javascript/components/Tweets/Tags.js
@@ -1,31 +1,28 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Tag from './Tag'
 
-class Tags extends React.Component {
-  render(){
-    const tags = this.props.taggings.map((tagging) => {
-      return(
-        <Tag
-          name={tagging.tag_name}
-          key={tagging.id}
-          id={tagging.id}
-          taggingId={tagging.id}
-          tagging={tagging}
-          likedTweetId={tagging.liked_tweet_id}
-          deleteTagging={this.props.deleteTagging}
-          filterByTagName={this.props.filterByTagName}
-        />
-      )
-    })
-
-    return (
-      <React.Fragment>
-        <div className="tags-container">
-          Tags: {tags}
-        </div>
-      </React.Fragment>
+const Tags = (props) => {
+  const tags = props.taggings.map((tagging) => {
+    return(
+      <Tag
+        name={tagging.tag_name}
+        key={tagging.id}
+        id={tagging.id}
+        taggingId={tagging.id}
+        tagging={tagging}
+        likedTweetId={tagging.liked_tweet_id}
+        deleteTagging={props.deleteTagging}
+        filterByTagName={props.filterByTagName}
+      />
     )
-  }
+  })
+  return (
+    <>
+      <div className="tags-container">
+        Tags: {tags}
+      </div>
+    </>
+  )
 }
 
 export default Tags
